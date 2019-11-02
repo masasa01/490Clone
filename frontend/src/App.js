33173cd1eebc229df65ba0a43635ebf52dc20214
Samson Masanga <32495066+masasa01@users.noreply.github.com>
2019-11-02 16:03:49 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,6 @@ function App() {
   return (
     <div>
       <p>Hello World</p>
-      <router>
-        <ul>
-          <li>
-            <link to="/About">About</link>
-          </li>
-          <li>
-            <link to="/Table">Table</link>
-          </li>
-        </ul>
-      </router>
     </div>
   );
 }
