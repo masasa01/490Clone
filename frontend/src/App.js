2d1668757503c59f1ac2bbf7c96ba3bff763961a
Samson Masanga <32495066+masasa01@users.noreply.github.com>
2019-11-11 23:35:16 -0600

    frontend stuff
    added bootstrap stuff. Table and About components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import Axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.css';
 import NavBar from './components/NavBar.jsx';
+import TableComponent from './components/TableComponent.jsx';
+import About from './components/About.jsx';
 
 
 class App extends Component {
@@ -10,6 +12,8 @@ class App extends Component {
       return (
         <div>
           <NavBar />
+          <About />
+          <TableComponent />
 
         </div>
       )
---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+
+
+class About extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Tutor Hub</h1>
+                <Carousel>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="./network.jpg"
+                        alt="We strive in exellence"
+                        />
+                        <Carousel.Caption>
+                        <h3>World Class tutors</h3>
+                        <p>Our Tutors are committed to helping you succeed.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="./././pictures/tutoring-photo-1"
+                        alt="A massive network"
+                        />
+
+                        <Carousel.Caption>
+                        <h3>We have tutors from all over the country</h3>
+                        <p>We recruit tutors from all over the country, to help you be at your best wherever you are.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="./././pictures/tutoring-photo-1"
+                        alt="Guaranteed Success"
+                        />
+
+                        <Carousel.Caption>
+                        <h3>Tutee Results</h3>
+                        <p>Our tutees report high success rates in tough courses like the IB program.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+            </div>
+        )
+    }
+}
+
+export default About;
