0bdbb299bfbd9fd076bcf7e20858b890062fd25e
Samson Masanga <32495066+masasa01@users.noreply.github.com>
2019-11-15 22:02:52 -0600

    Fixing Dev

--- backend/app.js
+++ backend/app.js
@@ -1,64 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
+require('dotenv').config();
 
-var express = require('express');
-var app = express();
-var router = express.Router();
-var cors = require('cors');
-var db = require('./database');
-const bodyParser = require('body-parser');
-const logger = require('morgan');
+const app = express();
+const port = process.env.PORT || 3001;
 
 app.use(cors());
+app.use(express.json());
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(logger('dev'));
-
-var name;
-var location;
-var time;
-var subject;
-var form_fields = {name, location, time, subject};
-var formdata = [];
-
-router.get("/get", (request, response) => {
-  var output = formdata.pop();
-  return response.json({ output });
-});
-
-router.post('/post', (request, response) => {
-  name=request.body.name;
-  location=request.body.location;
-  time=request.body.time;
-  subject=request.body.subject;
-
-  db.conn(function(err,dbo){
-    if(err){
-      response.sendStatus(500);
-      console.log("My guy, you got to connect to the database first");
-    }
-
-
-  return response.json({ success: "We Posted" });
-});
-
-router.get("/get", (request, response) => {
-  var output = formdata.pop();
-  return response.json({ output });
-});
-
-router.post('/post', (request, response) => {
-  form_fields.name = request.body.name;
-  form_fields.id = request.body.id;
-  formdata.push(form_fields);
-
-  return response.json({ success: "We Posted" });
-});
-
-
-app.use('/api', router);
-
-app.listen(3001, function () {
-  console.log('Example app listening on port 3001!');
-});
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology:true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
 })
+
+const tutorRouter = require("./routes/tutors");
+app.use('/tutorInfo', tutorRouter);
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
